1592ea98954373a519669439b5b3434fbcbf945e
Nahuel Barrios <barrios.nahuel@gmail.com>
2013-12-15 22:44:30 -0300

    Add Phonegap files to the main project and for mock ups too.

---
+++ maquetas/platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/justkenchu/Coding/workspace/misPacientes/maquetas/platforms/android/bin/AndroidManifest.xml : \
+/home/justkenchu/Coding/workspace/misPacientes/maquetas/platforms/android/AndroidManifest.xml \

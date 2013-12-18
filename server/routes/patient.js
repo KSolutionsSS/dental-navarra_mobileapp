27449c75bb39bd2fd4e486b7331aa5ace8313bf5
Nahuel Barrios <barrios.nahuel@gmail.com>
2013-12-17 21:19:21 -0300

    Clean patient object by removing unnecessary tokens attribute.

--- server/routes/patient.js
+++ server/routes/patient.js
@@ -5,34 +5,34 @@
 exports.findAll = function (req, res) {
     res.send([
                  {
-                     id: 1, name: 'Nahuel', lastName: 'Barrios', tokens: 'Nahuel Barrios'.split(' ')
+                     id: 1, name: 'Nahuel', lastName: 'Barrios'
                  },
                  {
-                     id: 2, name: 'Gustavo', lastName: 'Vignolo', tokens: 'Gustavo Vignolo'.split(' ')
+                     id: 2, name: 'Gustavo', lastName: 'Vignolo'
                  },
                  {
-                     id: 3, name: 'Nicolas', lastName: 'Vignolo', tokens: 'Nicolas Vignolo'.split(' ')
+                     id: 3, name: 'Nicolas', lastName: 'Vignolo'
                  },
                  {
-                     id: 4, name: 'Carolina', lastName: 'Vignolo', tokens: 'Carolina Vignolo'.split(' ')
+                     id: 4, name: 'Carolina', lastName: 'Vignolo'
                  },
                  {
-                     id: 5, name: 'Patricia', lastName: 'Safranchik', tokens: 'Patricia Safranchik'.split(' ')
+                     id: 5, name: 'Patricia', lastName: 'Safranchik'
                  },
                  {
-                     id: 6, name: 'Paola', lastName: 'Safranchik', tokens: 'Paola Safranchik'.split(' ')
+                     id: 6, name: 'Paola', lastName: 'Safranchik'
                  },
                  {
-                     id: 7, name: 'Claudia', lastName: 'Safranchik', tokens: 'Claudia Safranchik'.split(' ')
+                     id: 7, name: 'Claudia', lastName: 'Safranchik'
                  },
                  {
-                     id: 8, name: 'Cristian', lastName: 'Caputto', tokens: 'Cristian Caputto'.split(' ')
+                     id: 8, name: 'Cristian', lastName: 'Caputto'
                  },
                  {
-                     id: 9, name: 'Tomas', lastName: 'Caputto', tokens: 'Tomas Caputto'.split(' ')
+                     id: 9, name: 'Tomas', lastName: 'Caputto'
                  },
                  {
-                     id: 10, name: 'María Sol', lastName: 'Caputto', tokens: 'María Sol Caputto'.split(' ')
+                     id: 10, name: 'María Sol', lastName: 'Caputto'
                  }
              ]);
 };

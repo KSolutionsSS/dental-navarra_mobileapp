a953ee1bee3891623babe7f29d95ed70d4298f31
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-02-10 01:45:45 -0300

    Add android-support-v4.jar as dependency to display notifications

--- platforms/android/src/main/java/com/red_folder/phonegap/plugin/backgroundservice/MyService.java
+++ platforms/android/src/main/java/com/red_folder/phonegap/plugin/backgroundservice/MyService.java
@@ -1,6 +1,14 @@
 package com.red_folder.phonegap.plugin.backgroundservice;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
+import com.nbempire.dentalnavarra.DentalNavarraActivity;
+import com.nbempire.dentalnavarra.R;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -18,6 +26,7 @@ public class MyService extends BackgroundService {
         Log.i(TAG, "--> doWork");
         JSONObject result = new JSONObject();
 
+
         try {
             SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
             String now = df.format(new Date(System.currentTimeMillis()));
@@ -25,12 +34,44 @@ public class MyService extends BackgroundService {
             String msg = "Hello " + this.mHelloTo + " - its currently " + now;
             result.put("Message", "{\"id\":\"hola\", \"value\":\"nahuel\", \"mensaje\":\"" + msg + "\"}");
 
+//            showNotification();
+
             Log.d(TAG, msg);
         } catch (JSONException e) {
             Log.e(TAG, e.getMessage());
         }
 
         return result;
+    }
+
+    private void showNotification() {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.notification)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!");
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, DentalNavarraActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(DentalNavarraActivity.class);
+
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(44, mBuilder.build());
+
+        Log.d(TAG, "ya tire la notificacion");
     }
 
     @Override
